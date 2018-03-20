665ce5750f6e51955ff2a25fb999d103983baf7e
Louis Boehling <louisboehling@gmail.com>
2018-03-20 11:59:05 -0400

    css update

--- javascript.js
+++ javascript.js
@@ -57,16 +57,48 @@ database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
     console.log(sv.arrivalTime);
     console.log(sv.frequency);
 
+// Moment.js (has to be in the database function)
+
+var tFrequency = sv.frequency;
+var firstTime = sv.arrivalTime;
+
+// First Time (pushed back 1 year to make sure it comes before current time)
+var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+console.log(firstTimeConverted);
+
+// Current Time
+var currentTime = moment();
+console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+// Difference between the times
+var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+console.log("DIFFERENCE IN TIME: " + diffTime);
+
+// Time apart (remainder)
+var tRemainder = diffTime % tFrequency;
+console.log(tRemainder);
+
+// Minute Until Train
+var tMinutesTillTrain = tFrequency - tRemainder;
+console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+// Next Train
+var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm A"));
+
+var nextTrainArrival = (moment(nextTrain).format("hh:mm A"));
+
+
 // Makes a new row in the table 
 
 var tableRow =
 `
 <tr>
-    <th scope="row">${sv.name}</th>
+    <th scope="row">${sv.train}</th>
     <td>${sv.destination}</td>
     <td>${sv.frequency}</td>
     <td>${sv.arrivalTime}</td>
-   <td></td>
+    <td>${tMinutesTillTrain}</td>
 </tr>
 `
 $('tbody').append(tableRow);
